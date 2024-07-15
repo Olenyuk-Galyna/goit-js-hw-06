457a3815778a416c485f7c2204d736a93f3ecf3e
galyna <galaibidag@gmail.com>
2024-07-15 21:05:00 +0300

    add index file

--- js/task-2.js
+++ js/task-2.js
@@ -1,3 +1,19 @@
+class Storage {
+  #items;
+  constructor(arr) {
+    this.#items = arr;
+  }
+  getItems() {
+    return this.#items;
+  }
+  addItem(newItem) {
+    this.#items.push(newItem);
+  }
+  removeItem(itemToRemove) {
+    this.#items = this.#items.filter((el) => el !== itemToRemove);
+  }
+}
+
 const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
 console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
 
